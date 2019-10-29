3d7b6d3ad3c48da60a592fbc6f3ff9169cb49ef1
cuichaochao <cuichaochao916@gmail.com>
2019-10-29 16:38:00 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,13 +4,15 @@ DOTFILES_PATH=`pwd`
 cd $HOME
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-git config --global user.name "OSer"
+git config --global user.name "OSer916"
 git config --global user.email "cuichaochao916@gmail.com"
 git config --global core.editor vim
 git config --global merge.tool meld
 
 ln -s $DOTFILES_PATH/vimrc .vimrc
+ln -s $DOTFILES_PAH/bashrc.local .bashrc.local
 
-echo "source .basrc.local" >> $HOME/.bashrc
-ln -s $DOTFILES_PATH/bashrc.local
+echo "source .bashrc.local" >> $HOME/.bashrc
+
+sudo apt install -y vim uncrustify gitlint
 
