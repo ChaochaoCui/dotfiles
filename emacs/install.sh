d5773e2440cb88d8d0a485bb0c6ea035fa957b80
Chaochao Cui <cuichaochao916@gmail.com>
2022-06-26 21:33:21 +0800

    Update emacs

--- emacs/install.sh
+++ emacs/install.sh
@@ -2,14 +2,19 @@
 
 set -eu
 
+NAME=emacs
+
 if [[ -e .done ]]; then
-	echo "Emacs has Installed!"
+	echo "$NAME has Installed!"
 else
 	git clone https://github.com/ChaochaoCui/doom.d ~/.doom.d
+	cd ~/.doom.d/
+	git remote set-url origin git@github.com:ChaochaoCui/doom.d.git
+	cd -
 	git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
 	export PATH=~/.emacs.d/bin:$PATH
 	doom install --force
 	echo "export PATH=~/.emacs.d/bin:\$PATH" >> ~/.bashrc
 	touch .done
-	echo "Install Emacs Done"
+	echo "Install $NAME Done"
 fi
