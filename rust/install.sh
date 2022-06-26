ad97e51c96e49d97ca2a6ec90a158f711fdb1317
Chaochao Cui <cuichaochao916@gmail.com>
2022-06-26 21:46:10 +0800

    Update rust

--- rust/install.sh
+++ rust/install.sh
@@ -8,6 +8,9 @@ if [[ -e .done ]]; then
 	echo " $NAME has Installed!"
 else
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	rustup component add rust-src
+	curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > $BINS_DIR/rust-analyzer
+	chmod a+x $BINS_DIR/rust-analyzer
 	touch .done
 	echo "Install $NAME Done"
 fi
